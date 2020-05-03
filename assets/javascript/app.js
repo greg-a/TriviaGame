b693ccb2640b10144a8b2431efd471ad6ce44a6d
Greg Allebach <greg.allebach@gmail.com>
2020-05-03 19:29:43 -0400

    added countdown function and next question function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,3 @@
-// create start screen with a start button
-// after starting, show question with multiple choice and countdown from 30 seconds
-// create three scenarios: 1) correct guess(shows congratulation for 3 seconds), 2) incorrect guess, 3) timeout
-// end screen that shows: correct tally, incorrect tally, and unanswered tally.
-// start over button that starts new round of questions 
-
 var correctGuesses = 0;
 var incorrectGuesses = 0;
 var unansweredQuestions = 0;
@@ -12,23 +6,87 @@ var questions = [
     questionA = {
         question: "What is 2 + 2?",
         answer: "4",
-        options: {
-            line1: "8",
-            line2: "4",
-            line3: "2",
-            line4: "10"
-        }
+        options: [
+            line1 = {
+                box: $("<div class='option'>"),
+                option: "8"
+            },
+            line2 = {
+                box: $("<div class='option'>"),
+                option: "12"
+            },
+            line3 = {
+                box: $("<div class='option'>"),
+                option: "4"
+            },
+            line4 = {
+                box: $("<div class='option'>"),
+                option: "2"
+            },
+            
+        ]
     },
     questionB = {
         question: "What is 2 x 8?",
         answer: "16",
-        options: {
-            line1: "8",
-            line2: "4",
-            line3: "16",
-            line4: "10"
-        }
+        options: [
+            line1 = {
+                box: $("<div class='option'>"),
+                option: "8"
+            },
+            line2 = {
+                box: $("<div class='option'>"),
+                option: "12"
+            },
+            line3 = {
+                box: $("<div class='option'>"),
+                option: "4"
+            },
+            line4 = {
+                box: $("<div class='option'>"),
+                option: "0"
+            },
+            
+        ]
     }    
 ]
+var answerInt = 1;
+
+var intervalId;
+
+function nextQuestion() {
+
+    $(".button, h1").remove();
+    $(".question").append(questions[answerInt].question);
+    $(".countdown").append(countDown);
+
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+
+    for (var i = 0; i < questions[answerInt].options.length; i++) {
+        $(".choices").append(questions[answerInt].options[i].box);
+        $(questions[answerInt].options[i].box).addClass("box" + i);
+        $(".box" + i).append(questions[answerInt].options[i].option);
+
+    }
+}
 
 
+
+function decrement() {
+    countDown--;
+
+    $(".countdown").html("<h2>" + countDown + "</h2>");
+
+    if (number === 0) {
+        
+        unansweredQuestions++;
+        
+      // run wrong answer function
+
+    }
+  }
+
+$("#start-button").on("click", function() {
+    nextQuestion();
+})
