7854f357b41feb8d39b70d852155bffed8380ec0
Greg Allebach <greg.allebach@gmail.com>
2020-05-03 11:08:56 -0400

    initial commit with some pseudocoding

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+// create start screen with a start button
+// after starting, show question with multiple choice and countdown from 30 seconds
+// create three scenarios: 1) correct guess(shows congratulation for 3 seconds), 2) incorrect guess, 3) timeout
+// end screen that shows: correct tally, incorrect tally, and unanswered tally.
+// start over button that starts new round of questions 
